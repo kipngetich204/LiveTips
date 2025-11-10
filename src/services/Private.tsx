e6f4e25ffc2ae0fc34ca8649c757cf3d7ee668be
kipngetich204 <153486695+kipngetich204@users.noreply.github.com>
2025-11-10 17:51:49 +0300

    fixing protected auth for admin

--- src/services/Private.tsx
+++ src/services/Private.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
-import { useAuth } from "../hooks/useAuth";
+import { useAuth } from "../context/AuthContext";
 
 interface PrivateRouteProps {
   children: React.ReactNode;
